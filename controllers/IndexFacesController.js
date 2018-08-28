0c7b6a6696af853287025f9ad2c45e7f1d511e0f
arapocket <arakazaryan@me.com>
2018-08-28 10:30:08 -0700

    quick save

---
+++ controllers/IndexFacesController.js
@@ -0,0 +1,98 @@
+let fs = require('fs');
+let path = require('path');
+let AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' }); // TODO:can also use a global config onject in same way as credentials
+let rekognition = new AWS.Rekognition();
+const RcognizeGalleryModel = require('../models/RcognizeGalleryModel');
+
+
+if (process.send) {
+  process.send('child started');
+}
+
+process.on('message', message => {
+  let imagesIndexed = 0;
+  let numberOfImages = 0;
+
+  message.files.forEach(function (file, index) {
+
+    let fromPath = path.join(message.moveFrom, file);
+
+    // check if file is .png or .jpg
+    if (fromPath.slice(-3) === 'png' || fromPath.slice(-3) === 'jpg') {
+      numberOfImages++;
+
+      fs.readFile(fromPath, (err, data) => {
+        // Uploads image onto bucket first
+        let s3 = new AWS.S3();
+
+        s3.putObject({
+          Bucket: 'rekog-image-bucket',
+          Key: file,
+          //Body: base64data
+          Body: data,
+          // Tagging: 'test1=1&test2=hello',
+          // Metadata: {
+          //   'metadata1': 'test value 1',
+          //   'metadata2': 'Ara & Evan'
+          // }
+        }, function (err, data) {
+          if (err) console.log(err, err.stack);
+          else {
+            // Indexes faces into rekognition collection
+            let params = {
+              CollectionId: 'myTestImages',
+              DetectionAttributes: [
+              ],
+              ExternalImageId: file,
+              Image: {
+                S3Object: {
+                  Bucket: 'rekog-image-bucket',
+                  Name: file
+                }
+              }
+            };
+
+            rekognition.indexFaces(params, function (err, data) {
+              if (err) console.log(err, err.stack); // an error occurred
+              else {
+                imagesIndexed++;
+                if (data.FaceRecords.length > 0) {
+                  // successful response
+                  const faceData = data.FaceRecords[0].Face;
+                  const bucketName = 'rekog-image-bucket';
+                  const link = `https://s3.console.aws.amazon.com/s3/object/${bucketName}/${faceData.ExternalImageId}?region=us-east-1&tab=overview`;
+                  const body = {
+                    Link: link,
+                    Tag: '',
+                    FaceID: faceData.FaceId,
+                    Name: faceData.ExternalImageId.slice(0,-4)
+                  };
+                  RcognizeGalleryModel.postIndexedPhoto(body);
+                }
+                console.log(JSON.stringify(data));
+                if (imagesIndexed === numberOfImages) {
+                  callback();
+                }
+              }
+            });
+          }
+        });
+      });
+    }
+  });
+
+
+  function callback() {
+    console.log('done');
+    // createLogEntry('done'); 
+  }
+
+  // function createLogEntry(param) {
+  //   fs.open('./public/reports/photoProcess.log', 'a', 666, function (e, fd) {
+  //     fs.appendFileSync(fd, param + "\r\n", null, 'utf8')
+  //     fs.close(fd, function () { });
+  //     return;
+  //   });
+  // }
+});
--- models/RcognizeGalleryModel.js
+++ models/RcognizeGalleryModel.js
@@ -1,43 +1,43 @@
 let db = require('./db');
 
 module.exports.postIndexedPhoto = function (body, callback) {
-  db.createConnection(function (err, connection) {
+  db.createConnection(function (err, postIndexedPhotoResult) {
     if (err) {
       console.log('Error while performing common connect query: ' + err);
       callback(err, null);
     } else {
       //process the i/o after successful connect.  Connection object returned in callback
 
-      connection('face')
-        .insert({ body })
-        .then(result => {
-          console.log(result);
-          connection.destroy();
-          callback(null, result);
-        })
-        .catch(err => {
-          console.log(err);
-          connection.destroy();
-          callback(err, null);
-        });
-
-
-      // let { Link, Tag, FaceID, Name } = body;
-      // let queryFields = '(Link, Tag, FaceID, Name)';
-      // let queryValues = `('${Link}', '${Tag}', '${FaceID}', '${Name}')`;
-      // let query = 'INSERT INTO face ' + queryFields + ' VALUES ' + queryValues;
-      // connection.query(query, function (err, rows, fields) {
-      //   if (!err) {
-      //     connection.end();
-      //     // callback(null, rows);
-
-      //   } else {
-      //     console.log('error with the postIndexedPhoto query');
+      // connection('face')
+      //   .insert({ body })
+      //   .then(result => {
+      //     console.log(result);
+      //     connection.destroy();
+      //     callback(null, result);
+      //   })
+      //   .catch(err => {
       //     console.log(err);
-      //     connection.end();
-      //     // callback(err, rows);
-      //   }
-      // });
+      //     connection.destroy();
+      //     callback(err, null);
+      //   });
+
+      let { Link, Tag, FaceID, Name } = body;
+      let connection = postIndexedPhotoResult;
+      let queryFields = '(Link, Tag, FaceID, Name)';
+      let queryValues = `('${Link}', '${Tag}', '${FaceID}', '${Name}')`;
+      let query = 'INSERT INTO face ' + queryFields + ' VALUES ' + queryValues;
+      connection.query(query, function (err, rows) {
+        if (!err) {
+          connection.end();
+          callback(null, rows);
+
+        } else {
+          console.log('error with the postIndexedPhoto query');
+          console.log(err);
+          connection.end();
+          callback(err, rows);
+        }
+      });
     }
   });
 };
