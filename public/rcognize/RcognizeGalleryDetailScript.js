ccc9f3ef3326762e681f605addae94e824e60684
arapocket <arakazaryan@me.com>
2018-08-30 10:01:27 -0700

    quick save

--- public/rcognize/RcognizeGalleryDetailScript.js
+++ public/rcognize/RcognizeGalleryDetailScript.js
@@ -3,6 +3,11 @@ function initScript(){
     console.log(results[0]);
 
     let incidentPhoto = document.getElementById("incidentPhoto");
-    
     incidentPhoto.src = results[0].Link;
+
+    let addButton = document.getElementById('addButton');
+
+    addButton.addEventListener('click', function (){
+        console.log('lick my nuts');
+    })
 }
